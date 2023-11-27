e0e485dabd7a6ddeb68a3382376d7cee58eb44b9
Stalloyde <stalloyde93@gmail.com>
2023-11-27 14:45:00 +0800

    Remove athletes and staff properties

--- models/sport.js
+++ models/sport.js
@@ -4,8 +4,6 @@ const { Schema } = mongoose;
 
 const SportSchema = new Schema({
   name: { type: String, required: true },
-  athletes: { type: mongoose.Schema.Types.ObjectId, ref: 'Athlete', required: true },
-  staff: { type: mongoose.Schema.Types.ObjectId, ref: 'Staff', required: true },
 });
 
 SportSchema.virtual('url').get(() => `/sport/${this.name}`);
