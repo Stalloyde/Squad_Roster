67e61ccf982f314d7d09fcfc59853be7cd592047
Stalloyde <stalloyde93@gmail.com>
2023-11-27 11:05:17 +0800

    Add sex property

--- models/athlete.js
+++ models/athlete.js
@@ -6,6 +6,7 @@ const { Schema } = mongoose;
 const AthleteSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
+  sex: { type: String, required: true },
   height: { type: Number, required: true },
   weight: { type: Number, required: true },
   sport: { type: mongoose.Schema.Types.ObjectId, ref: 'Sport', required: true },
