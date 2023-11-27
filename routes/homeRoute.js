590734fcac4e7fd57bded0f7b5f7954e3c3e59ae
Stalloyde <stalloyde93@gmail.com>
2023-11-27 23:53:23 +0800

    Add and style homepage and logo

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const asyncHandler = require('express-async-handler');
+const Athlete = require('../models/athlete');
+const Staff = require('../models/staff');
+const Sport = require('../models/sport');
 
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('home', { title: 'Express' });
-});
+router.get('/', asyncHandler(async (req, res, next) => {
+  const [athleteCount, staffCount, sportCount] = await Promise.all([
+    Athlete.find().count(),
+    Staff.find().count(),
+    Sport.find().count(),
+  ]);
+
+  res.render('home', {
+    athleteCount, staffCount, sportCount,
+  });
+}));
 
 module.exports = router;
