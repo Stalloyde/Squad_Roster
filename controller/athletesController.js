e651a414d1a5e3c41527693800e17b080733c963
Stalloyde <stalloyde93@gmail.com>
2023-12-05 15:47:39 +0800

    Implement edit and delete athlete features

--- controller/athletesController.js
+++ controller/athletesController.js
@@ -18,7 +18,7 @@ exports.athleteDetails = asyncHandler(async (req, res, next) => {
 
 exports.newAthleteGET = asyncHandler(async (req, res, next) => {
   const sports = await Sport.find().sort({ name: 1 });
-  res.render('./athletes/new-athlete', { sports });
+  res.render('./athletes/athlete-form', { sports });
 });
 
 exports.newAthletePOST = [
@@ -48,10 +48,80 @@ exports.newAthletePOST = [
 
     if (!errors.isEmpty()) {
       const sports = await Sport.find();
-      res.render('./athletes/new-athlete', { sports, athlete: newAthlete, errors: errors.array() });
+      res.render('./athletes/athlete-form', {
+        sports, athlete: newAthlete, errors: errors.array(),
+      });
     } else {
       await newAthlete.save();
       res.redirect(newAthlete.url);
+    }
+  }),
+];
+
+exports.editAthleteDetailsGET = asyncHandler(async (req, res, next) => {
+  const [sports, targetAthlete] = await Promise.all([
+    Sport.find().sort({ name: 1 }),
+    Athlete.findById(req.params.id).populate('sport'),
+  ]);
+  res.render('./athletes/athlete-form', { sports, athlete: targetAthlete, title: `Edit Particulars - ${targetAthlete.fullName}` });
+});
+
+exports.editAthleteDetailsPOST = [
+  body('firstName').trim().escape(),
+  body('lastName').trim().escape(),
+  body('sex.*').escape(),
+  body('height').isNumeric().escape(),
+  body('weight').isNumeric().escape(),
+  body('sport').trim().escape(),
+  body('dob').isISO8601().toDate()
+    .escape(),
+  body('password').equals(process.env.PASSWORD)
+    .withMessage('Password incorrect. Please try again.'),
+
+  asyncHandler(async (req, res, next) => {
+    const newAthlete = new Athlete({
+      id: req.params.id,
+      firstName: capitalise(req.body.firstName),
+      lastName: capitalise(req.body.lastName),
+      sex: req.body.sex,
+      height: req.body.height,
+      weight: req.body.weight,
+      sport: req.body.sport,
+      dateOfBirth: req.body.dob,
+    });
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      const sports = await Sport.find();
+      res.render('./athletes/athlete-form', {
+        sports, athlete: newAthlete, errors: errors.array(),
+      });
+    } else {
+      const updatedAthlete = await Athlete.findOneAndUpdate({ _id: req.params.id }, newAthlete);
+      res.redirect(updatedAthlete.url);
+    }
+  }),
+];
+
+exports.deleteAthleteGET = asyncHandler(async (req, res, next) => {
+  const targetAthlete = await Athlete.findById(req.params.id);
+  res.render('./athletes/athlete-delete', { athlete: targetAthlete });
+});
+
+exports.deleteAthletePOST = [
+  body('password').equals(process.env.PASSWORD)
+    .withMessage('Password incorrect. Please try again.'),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const targetAthlete = await Athlete.findById(req.params.id);
+    if (!errors.isEmpty()) {
+      res.render('./athletes/athlete-delete', { athlete: targetAthlete, errors: errors.array() });
+    } else {
+      await Athlete.findByIdAndDelete(req.params.id);
+      res.redirect('/athletes');
     }
   }),
 ];
--- routes/athletesRoute.js
+++ routes/athletesRoute.js
@@ -7,5 +7,9 @@ router.get('/', athletesController.athletesDirectory);
 router.get('/create', athletesController.newAthleteGET);
 router.post('/create', athletesController.newAthletePOST);
 router.get('/:id', athletesController.athleteDetails);
+router.get('/:id/edit', athletesController.editAthleteDetailsGET);
+router.post('/:id/edit', athletesController.editAthleteDetailsPOST);
+router.get('/:id/delete', athletesController.deleteAthleteGET);
+router.post('/:id/delete', athletesController.deleteAthletePOST);
 
 module.exports = router;
