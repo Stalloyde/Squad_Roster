91182111fd2d7bc699f248c62e41ffad61d04e6a
Stalloyde <stalloyde93@gmail.com>
2023-12-08 12:01:12 +0800

    Deployment prep

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const compression = require('compression');
+const helmet = require('helmet');
+const RateLimit = require('express-rate-limit');
 
 const indexRouter = require('./routes/index');
 const homeRouter = require('./routes/homeRoute');
@@ -12,10 +15,18 @@ const staffRouter = require('./routes/staffRoute');
 
 const app = express();
 
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 2000,
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(limiter);
+app.use(helmet());
+app.use(compression());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', false);
 const router = express.Router();
-const mongoDB = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@${process.env.COLLECTION}.uqtjxjp.mongodb.net/${process.env.COLLECTION}?retryWrites=true&w=majority`;
+
+const devConnectionString = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@${process.env.COLLECTION}.uqtjxjp.mongodb.net/${process.env.COLLECTION}?retryWrites=true&w=majority`;
+const mongoDB = devConnectionString || process.env.MONGODB_URI;
 
 async function main() {
   await mongoose.connect(mongoDB);
