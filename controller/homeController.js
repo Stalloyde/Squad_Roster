f43c1c0301ab964e009537039affc1ae7390e836
Stalloyde <stalloyde93@gmail.com>
2024-08-02 11:04:02 +0800

    Refactor to postgresql

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,8 +1,14 @@
-const express = require('express');
 const asyncHandler = require('express-async-handler');
 const queries = require('../db/queries');
 
 exports.homePage = asyncHandler(async (req, res, next) => {
-  const results = await queries.getAllInfo();
-  res.send(results);
+  const [athleteCount] = await queries.getAthleteCount();
+  const [sportCount] = await queries.getSportCount();
+  const [staffCount] = await queries.getStaffCount();
+
+  return res.render('./home', {
+    athleteCount: athleteCount.count,
+    sportCount: sportCount.count,
+    staffCount: staffCount.count,
+  });
 });
--- db/queries.js
+++ db/queries.js
@@ -5,14 +5,25 @@ const pool = new Pool({
   connectionString: `postgresql://${process.env.USERNAME}:${process.env.PASSWORD}@localhost:5432/${process.env.DATABASE}`,
 });
 
-async function getAllInfo() {
-  const result = await pool.query(
-    'SELECT firstName, lastName,sport FROM staff',
-  );
-  return result;
+async function getAthleteCount() {
+  const { rows } = await pool.query('SELECT count(*) FROM athletes');
+  return rows;
 }
+
+async function getSportCount() {
+  const { rows } = await pool.query('SELECT count(*) FROM sports');
+  return rows;
+}
+
+async function getStaffCount() {
+  const { rows } = await pool.query('SELECT count(*) FROM staff');
+  return rows;
+}
+
 // write queries here
 
 module.exports = {
-  getAllInfo,
+  getAthleteCount,
+  getSportCount,
+  getStaffCount,
 };
