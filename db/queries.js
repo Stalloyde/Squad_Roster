16c953ecf99069ba29ec7b4187af98c88f8afc8f
Stalloyde <stalloyde93@gmail.com>
2024-08-07 14:54:10 +0800

    Add production db connection string

--- db/queries.js
+++ db/queries.js
@@ -3,7 +3,9 @@ const { Pool } = require('pg');
 const capitalise = require('../controller/capitalise');
 
 const pool = new Pool({
-  connectionString: `postgresql://${process.env.USERNAME}:${process.env.PASSWORD}@localhost:5432/${process.env.DATABASE}`,
+  connectionString:
+    `postgresql://${process.env.USERNAME}:${process.env.PASSWORD}@localhost:5432/${process.env.DATABASE}` ||
+    process.env.PROD_DB,
 });
 
 async function getAthleteCount() {
