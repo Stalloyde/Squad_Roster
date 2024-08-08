715f86a65e68fbeb2809fe4f12dbfe0f0e3aae12
Stalloyde <stalloyde93@gmail.com>
2024-08-08 15:32:31 +0800

    test

--- db/queries.js
+++ db/queries.js
@@ -5,7 +5,7 @@ const capitalise = require('../controller/capitalise');
 // const devConnectionString = `postgresql://${process.env.USERNAME}:${process.env.PASSWORD}@localhost:5432/squad_roster`;
 
 const pool = new Pool({
-  connectionString: process.env.PROD_DB,
+  connectionString: process.env.DATABASE_URL,
 });
 
 async function getAthleteCount() {
